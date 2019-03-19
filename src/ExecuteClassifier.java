f0b861b53813c832763560052beb2c27399911be
Divin Ndayishima <divin.ndayishima@outlook.com>
2019-03-19 17:32:36 +0100

    First test Weka

--- src/ExecuteClassifier.java
+++ src/ExecuteClassifier.java
@@ -1,10 +1,23 @@
+import java.io.File;
+import java.io.IOException;
 
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
 public class ExecuteClassifier {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 
-
+	public static void main(String[] args) {		 
+		ArffLoader arff = new ArffLoader();
+		File file = new File("../optdigits.arff");
+		try {
+			arff.setFile(file);
+			Instances data = arff.getDataSet();
+		    for(Instance inst : data){
+		    	System.out.println("Instance:" + inst);
+		    }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
